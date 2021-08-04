6bb181ee90ab947cb8883cd555c7d8baa92e7011
PriyanshuG13 <priyanshu.gupta1312@gmail.com>
2021-08-05 00:29:09 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Head from 'next/head';
+import '../styles/globals.css';
+
+
+function HomePage() {
+  return <div>Welcome to Next.js!</div>
+}
+
+export default HomePage
